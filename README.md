df2fdb4f405dec558e06f9da781cdc47f65e7f56
Balázs Indig <dlazesz@users.noreply.github.com>
2021-02-04 15:09:28 +0100

    Total rewrite: add argument parser and new API functions

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# multihash
+# Multihash
+
+Calculate multiple hash digests for a piece of data in parallel, one algo/thread.
+
+Based on the code of Peter Wu <peter@lekensteyn.nl> (https://git.lekensteyn.nl/scripts/tree/digest.py).
+
+# Usage
+
+## From CLI
+
+Add data over STDIN and/or as arguments and select the desired algorithms:
+
+```bash
+cat data.txt | python3 digest.py -i data2.txt - --sha1 --sha256 -o checksums.txt
+```
+
+At least one algorithm is mandatory and by default the script reads from STDIN and writes to STDOUT.
+
+## From Python
+
+### The exposed API is the following
+
+- `ALGORITHMS_GUARANTEED`: The tuple of the supported algorithms
+- `Hasher()`: Single-threaded hasher, takes an iterable (e.g. list of algorithms to use)
+- `MtHasher()` Multi-threaded hasher, takes an iterable (e.g. list of algorithms to use)
+
+### Both hashers expose the following API
+
+- `header`: tuple of header elements ("filename" and the list of algorithms in the supplied order)
+- `algos`: tuple of supplied algorithms
+- `hash_file()`: Takes a filename or a file-like object on bytes, returns the digest tuple in same order as header (the filename is omited)
+- `hash_multiple_files()`:Takes an iterable of filenames or file-like objects on bytes,returns the generator of filename + digest tuples in same order as header, one for every input object
+
+### Example
+
+```python
+from io import BytesIO
+
+from digest import MtHasher
+
+hasher = MtHasher(('sha1', 'md5'))
+filename_header, sha1_header, md5_header = hasher.header
+sha1_digest, md5_digest = hasher.hash_file('data.txt')
+for filename, sha1_digest, md5_digest in hasher.hash_multiple_files(('data.txt', open('data2.txt', 'rb'), '-', BytesIO('bytesstring'))):
+    # First the header and then the digests
+    print(filename, sha1_digest, md5_digest, sep='\t')
+```
+
+## Supported algorithms
+
+- md5
+- sha1
+- sha224
+- sha256
+- sha384
+- sha512
+- sha3_224
+- sha3_256
+- sha3_384
+- sha3_512
+- blake2b
+- blake2s
+
+# License
+
+Licensed under the MIT license <http://opensource.org/licenses/MIT>
--- digest.py
+++ digest.py
@@ -1,65 +1,113 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Calculate (multiple) digest(s) for file(s)
 #
-# Author: Peter Wu <peter@lekensteyn.nl>
+# Original Author: Peter Wu <peter@lekensteyn.nl>
 # Licensed under the MIT license <http://opensource.org/licenses/MIT>
 # Original source: https://git.lekensteyn.nl/scripts/tree/digest.py
 
 import sys
 import hashlib
+from io import BufferedReader, BytesIO
 from threading import Thread
 from queue import Queue
+from argparse import ArgumentParser, FileType
+
+# All guaranteed, except varable length hashes...
+ALGORITHMS_GUARANTEED = tuple(sorted(hashlib.algorithms_guaranteed - {'shake_128', 'shake_256'}))
 
 
-def read_blocks(filename):
-    if filename == '-':
-        f = sys.stdin.buffer  # read binary instead of unicode
-    else:
-        f = open(filename, 'rb')
-    try:
-        megabyte = 2 ** 20
-        data = f.read(megabyte)
-        while len(data) > 0:
-            yield data
-            data = f.read(megabyte)
-    finally:
-        f.close()
-
-
-class Hasher(object):
+class Hasher:
     """Calculate multiple hash digests for a piece of data"""
 
-    def __init__(self, algos):
-        self.algos = algos
-        self._hashes = {}
-        for algo in self.algos:
-            self._hashes[algo] = getattr(hashlib, algo)()
+    def __init__(self, algos=ALGORITHMS_GUARANTEED):
+        if not set(algos).issubset(ALGORITHMS_GUARANTEED) or len(set(algos)) != len(algos):
+            raise ValueError(f'The provided algorithms should not contain duplicates and '
+                  f'must be a subset of {ALGORITHMS_GUARANTEED}!')
+        header = list(algos)
+        header.insert(0, 'filename')
+        self.header = tuple(header)  # First element is the filename, then come the names of the algos
+        self.algos = tuple(header[1:])
 
-    def update(self, data):
-        for h in self._hashes:
+        self._hashers = {}
+        self._init_hashers()
+
+    def _init_hashers(self):
+        self._hashers = {}
+        for algo in self.algos:
+            self._hashers[algo] = getattr(hashlib, algo)()
+
+    def _update(self, data):
+        for h in self._hashers.values():
             h.update(data)
 
-    def header(self):
-        """First element is the filename, then come the names of the algos"""
-
-        algos = list(self.algos)
-        algos.insert(0, 'filename')
-        return tuple(algos)
-
-    def hexdigests(self):
+    def _hexdigests(self):
         """Returns the calculated hex digests"""
 
-        return (self._hashes[algo].hexdigest() for algo in self.algos)
+        return tuple(self._hashers[algo].hexdigest() for algo in self.algos)
+
+    @staticmethod
+    def _read_blocks(input_data, size=2**20):
+        """Read (one megabyte) blocks from a bytestream, STDIN or file"""
+
+        if isinstance(input_data, (BufferedReader, BytesIO)):
+            f = input_data
+            opened = False
+        elif input_data == '-':
+            f = sys.stdin.buffer  # read binary instead of unicode
+            opened = False
+        else:
+            f = open(input_data, 'rb')
+            opened = True
+
+        try:
+
+            data = f.read(size)
+            while len(data) > 0:
+                yield data
+                data = f.read(size)
+        finally:
+            if opened:
+                f.close()
+
+    def hash_file(self, filename_or_bytestream):
+        """Try to read the file or bytestream and update the hash states"""
+
+        try:
+            for data in self._read_blocks(filename_or_bytestream):
+                self._update(data)
+        except OSError as e:
+            print('digest: ', filename_or_bytestream, ': ', e.strerror, sep='', file=sys.stderr)
+            return None
+        return self._hexdigests()
+
+    def hash_multiple_files(self, inputs):
+        """Hash multiple files sequentially. Yield the header then a filename and digests tuple for each file"""
+
+        yield self.header
+        for filename_or_bytestream in inputs:  # Calculate digest(s) for each file
+            digests = self.hash_file(filename_or_bytestream)
+            if digests is not None:
+                yield (filename_or_bytestream, *digests)
 
 
 class MtHasher(Hasher):
+    """Calculate multiple hash digests for a piece of data in parallel, one algo/thread"""
+
     QUEUE_SIZE = 10  # Queue size. Memory usage is this times block size (1M)
 
-    def __init__(self, algos):
+    def __init__(self, algos=ALGORITHMS_GUARANTEED):
         super(MtHasher, self).__init__(algos)
         self._queues = {}
         self._threads = {}
-        for algo in algos:
+
+    def _init_threads(self):
+        """Clear hashers, queues and threds and init clean instances"""
+
+        self._init_hashers()
+        self._queues = {}
+        self._threads = {}
+
+        for algo in self.algos:
             t = Thread(target=self._queue_updater, args=(algo,), name=algo)
             self._queues[algo] = Queue(MtHasher.QUEUE_SIZE)
             self._threads[algo] = t
@@ -67,18 +115,19 @@ class MtHasher(Hasher):
 
     def _queue_updater(self, algo):
         q = self._queues[algo]
-        h = self._hashes[algo]
+        h = self._hashers[algo]
         data = q.get()
         while len(data) > 0:  # Treat an empty value as terminator
             h.update(data)
             data = q.get()
 
-    def update(self, data):
+    def _update(self, data):
+        """Put chunks from hash_file() into the queues"""
         if len(data) > 0:
             for q in self._queues.values():
                 q.put(data)
 
-    def hexdigests(self):
+    def _hexdigests(self):
         """Wait until all calculations are done and yield the results in meantime"""
 
         for algo in self.algos:
@@ -86,57 +135,40 @@ class MtHasher(Hasher):
             q.put(b'')  # Terminate
             self._threads[algo].join()
             assert q.empty()
-        return super(MtHasher, self).hexdigests()
+        return super(MtHasher, self)._hexdigests()
+
+    def hash_file(self, filename_or_bytestream):
+        """Init the threads and calls Hasher.hash_file() for the hashing"""
+        self._init_threads()
+        return super(MtHasher, self).hash_file(filename_or_bytestream)
 
 
-# All guaranteed, except varable length hashes...
-algorithms_guaranteed = hashlib.algorithms_guaranteed - {'shake_128', 'shake_256'}
+def parse_args():
+    parser = ArgumentParser(description='Calculate one or more hashes for one or more files, one algo/thread')
+    algo_group = parser.add_argument_group('Available hash algorithms')
+    for algo in ALGORITHMS_GUARANTEED:
+        algo_group.add_argument(f'--{algo}', help=f'{algo} hash algorithm', action='store_true', default=False)
+    parser.add_argument('-i', '--input', dest='input_files', nargs='+', default=['-'],
+                        help='Input files instead of STDIN (STDIN is denoted with -)', metavar='FILES')
+    parser.add_argument('-o', '--output', dest='output_stream',  type=FileType('w'), default=sys.stdout,
+                        help='Use output file instead of STDOUT', metavar='FILE')
 
-
-def print_usage():
-    dgst_opts = ' '.join('[-{0}]'.format(algo) for algo in algorithms_guaranteed)
-    print(f'Usage: python {sys.argv[0]} {dgst_opts} [FILE]...', file=sys.stderr)
-
-
-def main(*argv):
-    filenames = []
-    algos = set()
-
-    # TODO argparse
-    if any(help_arg in argv for help_arg in ('-h', '--help')):
-        print_usage()
-        return 1
-
-    for arg in argv:
-        if arg.startswith('-') and arg != '-':
-            algo = arg.lstrip('-')  # Strip leading '-'
-            if algo in algorithms_guaranteed:  # Preserve ordering, ignore duplicates
-                algos.add(algo)
-            else:
-                print('Unsupported algo:', algo, file=sys.stderr)
-        else:
-            filenames.append(arg)
-
+    opts = vars(parser.parse_args())
+    algos = tuple(algo for algo in ALGORITHMS_GUARANTEED if opts[algo])
     if len(algos) == 0:
-        print('Missing digest!', file=sys.stderr)
-        print_usage()
-        return 1
+        parser.print_help(sys.stderr)
+        exit(2)
 
-    if len(filenames) == 0:
-        filenames.append('-')  # Assume stdin if no file is given
+    return algos, tuple(opts['input_files']), opts['output_stream']
 
-    for filename in filenames:  # Calculate digest(s) for each file
-        hasher = MtHasher(algos)
-        try:  # Try to read the file and update the hash states
-            for data in read_blocks(filename):
-                hasher.update(data)
-        except OSError as e:
-            print('digest: ', filename, ': ', e.strerror, sep='')
-            continue
 
-        print(*hasher.header())
-        print(filename, *hasher.hexdigests(), sep='\t')
+def main():
+    algos, filenames, output_stream = parse_args()
+    hasher = MtHasher(algos)
+    for output_line in hasher.hash_multiple_files(filenames):
+        print(*output_line, sep='\t', file=output_stream)
+    output_stream.close()
 
 
 if __name__ == '__main__':
-    sys.exit(main(*sys.argv[1:]))
+    main()
