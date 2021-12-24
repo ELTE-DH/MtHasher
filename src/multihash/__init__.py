5aef1bb0999d669d7bcf01814033c1035a70c390
András Németh <sunarch@protonmail.com>
2021-12-24 13:30:40 +0100

    Restructure for packaging

---
+++ src/multihash/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8, vim: expandtab:ts=4 -*-
+
---
+++ src/multihash/version.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8, vim: expandtab:ts=4 -*-
+
+program_name = 'WAC Multihash'
+
+__version__ = '2.0.0'
+
